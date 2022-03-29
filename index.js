4197a67a3fe988ac70a68443b4d5186c29fa72c0
Samuel Banya <sbanya@fastmail.com>
2022-03-28 20:37:27 -0400

    Adding debug output changes

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+// NOTE: To run NodeJS's debugger in terminal, use the 'node inspect index.js' command
+// NOTE: Use the 'cont' command to step through code while using the debugger
+// NOTE: Use the '.exit' command to exit the debugger at any time
 let count = 1;
 while (count < 5) {
-  if (count === 1) {
-    console.log(`The loop has executed ${count} time.`);
-  } else {
-    console.log(`The loop has executed ${count} times.`);
-  }
-  count += 1;
+    debugger;
+    if (count === 1) {
+	console.log(`The loop has executed ${count} time.`);
+    } else {
+	console.log(`The loop has executed ${count} times.`);
+    }
+    count += 1;
+    debugger;
 }
